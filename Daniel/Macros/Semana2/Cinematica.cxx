8331f2a97a35449945a7360235b633532e224290
Daniel <danielvazquezlago@gmail.com>
2024-06-26 12:27:16 +0200

    26/06 Cinematica finalizada

--- Daniel/Macros/Semana2/Cinematica.cxx
+++ Daniel/Macros/Semana2/Cinematica.cxx
@@ -18,81 +18,6 @@
 #include "Clases.cxx"
 
 
-// Vamos a crear las funciones que usaremos despues para obtener los  resultados
-
-double t4(Colision colision, double theta4p)
-{
-    double m1 {colision.get_m1() * 938};
-    double m2 {colision.get_m2() * 938};
-    double m3 {colision.get_m3() * 938};
-    double m4 {colision.get_m4() * 938};
-    double t1 {colision.get_tbeam()};
-
-    double E1 {m1 + t1};
-    double p1 {pow(pow(E1, 2) - pow(m1, 2), 0.5)};
-    double E2 {m2};
-    double Etot {E1 + m2};
-    double Etotp {pow(pow(Etot, 2) - pow(p1, 2), 0.5)};
-
-    double gamma {Etot / Etotp};
-    double beta {p1 / Etot};
-
-
-    double E3p {(Etotp + (pow(m3, 2) - pow(m4, 2)) / Etotp) / 2};
-    double E4p {(Etotp + (pow(m4, 2) - pow(m3, 2)) / Etotp) / 2};
-
-    double p4p {pow(pow(E4p, 2) - pow(m4, 2), 0.5)};
-
-    double E4 {gamma * (E4p + beta * p4p * TMath::Cos(theta4p))};
-
-    double T4 {E4 - m4};
-
-    return T4;
-}
-
-double theta4(Colision colision, double theta4p)
-{
-    double m1 {colision.get_m1() * 931.494};
-    double m2 {colision.get_m2() * 931.494};
-    double m3 {colision.get_m3() * 931.494};
-    double m4 {colision.get_m4() * 931.494};
-    double t1 {colision.get_tbeam()};
-    // std::cout << "m4 : " << m4 << '\n';
-
-    double E1 {m1 + t1};
-    double p1 {pow(pow(E1, 2) - pow(m1, 2), 0.5)};
-    // std::cout << "E1 : " << E1 << '\n';
-    // std::cout << "p1 : " << p1 << '\n';
-    double E2 {m2};
-    double Etot {E1 + m2};
-    double Etotp {pow(pow(Etot, 2) - pow(p1, 2), 0.5)};
-
-    double gamma {Etot / Etotp};
-    double beta {p1 / Etot};
-    // std::cout << "Beta : " << beta << '\n';
-    // std::cout << "Gamma : " << gamma << '\n';
-
-    // double E3p {(Etotp + (pow(m3, 2) - pow(m4, 2)) / Etotp) / 2};
-    double E4p {(Etotp + (pow(m4, 2) - pow(m3, 2)) / Etotp) / 2};
-
-    double p4p {pow(pow(E4p, 2) - pow(m4, 2), 0.5)};
-
-    double E4 {gamma * (E4p + beta * p4p * TMath::Cos(theta4p))};
-    double p4 {pow(-pow(m4, 2) + pow(E4, 2), 0.5)};
-    // std::cout << "p4 : " << p4 << '\n';
-    // std::cout << "E4 : " << E4 << '\n';
-    // std::cout << "diff : " << E4 - p4 << '\n';
-
-    double theta4 {TMath::ACos((gamma / p4) * (p4p * TMath::Cos(theta4p) + beta * E4p))};
-    // std::cout << "theta4 : " << theta4 << '\n';
-
-    return theta4;
-}
-
-double theta4cm(Colision colision, double theta4cm)
-{
-    return 2;
-}
 
 // Función principal
 
@@ -100,51 +25,67 @@ void Cinematica()
 {
     // Definimos las partículas
     Particula li11;
-    Particula li12;
+    Particula li10;
     Particula d;
-    Particula p;
+    Particula t;
 
     // Detallamos las partículas
-    li11.definir_MZ(11, 3);
-    li11.Print();
-    li12.definir_MZ(12, 3);
-    d.definir_MZ(2, 1);
-    p.definir_MZ(1, 1);
+    li11.definir_AZ(11, 3,11.0437236);
+    li10.definir_AZ(10, 3,10.035483);
+    d.definir_AZ(2, 1,2.01410177784);
+    t.definir_AZ(3, 1,3.01604928132);
 
     // Definimos la colisión y la detallamos
     Colision colision {};
-    colision.introducir_particulas(li11, d, p, li12);
-    colision.energia(7.5 * 12);
+    colision.set_particulas(li11, d, t, li10);
+    colision.set_energia(7.5*12);
 
-    colision.GetParticle1().Print();
+    // colision.GetParticle1().Print();
 
 
-    auto* Li12 {new ActPhysics::Kinematics("11Li", "d", "p", 12 * 7.5)};
-    auto* g3Li12 {Li12->GetKinematicLine3()};
-    auto* g4Li12 {Li12->GetKinematicLine4()};
+    auto* Li10 {new ActPhysics::Kinematics("11Li", "d", "t", 12 * 7.5)};
+    auto* g3Li10 {Li10->GetKinematicLine3()};
+    auto* g4Li10 {Li10->GetKinematicLine4()};
 
-    auto* gother {new TGraph};
+    auto* gother10 {new TGraph};
+    auto* gother3 {new TGraph};
+
 
     for(double thetaCM = 0; thetaCM <= 180; thetaCM += 1)
-    {
-        auto Theta4 {theta4(colision, thetaCM * TMath::DegToRad())};
-        auto T4 {t4(colision, thetaCM * TMath::DegToRad())};
+    {   
+        auto [T3,Theta3] {valores3(li11, d, t, li10, 7.5 * 12,thetaCM * TMath::DegToRad())};
+        auto [T4,Theta4] {valores4(li11, d, t, li10, 7.5 * 12,thetaCM * TMath::DegToRad())};
         // std::cout << "Theta4: " << Theta4 << " T4 : " << T4 << '\n';
         if(std::isfinite(Theta4) && std::isfinite(T4))
-            gother->SetPoint(gother->GetN(), Theta4 * TMath::RadToDeg(), T4);
+            gother10->SetPoint(gother10->GetN(), Theta4 * TMath::RadToDeg(), T4);
+        if(std::isfinite(Theta3) && std::isfinite(T3))
+            gother3->SetPoint(gother3->GetN(), Theta3 * TMath::RadToDeg(), T3);
     }
     // g3Li10 -> Draw("al");
-    auto* canvas12 {new TCanvas {"canvas10", "Li10"}};
-    canvas12->DivideSquare(2);
-    canvas12->cd(1);
-    g4Li12->SetLineColor(kOrange);
-    g4Li12->SetLineWidth(4);
-    g4Li12->Draw("al");
+    auto* canvas10 {new TCanvas {"canvas10", "Li10"}};
+    canvas10->DivideSquare(4);
+    canvas10->cd(1);
+    g4Li10->SetLineColor(kOrange);
+    g4Li10->SetLineWidth(4);
+    g4Li10->SetTitle("Cinematica para 11Li(d,t);#theta_{4Lab} [#circ];E_{4Lab}");
+    g4Li10->Draw("al");
 
-    canvas12->cd(2);
-    gother->SetLineWidth(2);
-    gother->SetLineColor(kRed);
-    gother->SetMarkerStyle(24);
-    gother->SetMarkerColor(kBlue);
-    gother->Draw("apl");
+    canvas10->cd(2);
+    gother10->SetLineWidth(4);
+    gother10->SetLineColor(kRed);
+    gother10->SetTitle("Cinematica para 11Li(d,t);#theta_{4Lab} [#circ];E_{4Lab}");
+    gother10->Draw("al");
+
+    canvas10->cd(3);
+    g3Li10->SetLineColor(kOrange);
+    g3Li10->SetLineWidth(4);
+    g3Li10->SetTitle("Cinematica para 11Li(d,t);#theta_{3Lab} [#circ];E_{3Lab}");
+    g3Li10->Draw("al");
+
+    canvas10->cd(4);
+    gother3->SetLineWidth(4);
+    gother3->SetLineColor(kRed);
+    gother3->SetTitle("Cinematica para 11Li(d,t);#theta_{3Lab} [#circ];E_{3Lab}");
+    gother3->Draw("al");
 }
+
--- Daniel/Macros/Semana2/Clases.cxx
+++ Daniel/Macros/Semana2/Clases.cxx
@@ -17,18 +17,20 @@ class Particula
 {
     double fZ;
     double fA;
+    double fmasa_real;
 
 public:
-    void definir_MZ(double valorM, double valorZ);
-    double get_M() { return fA; };
+    void definir_AZ(double valorA, double valorZ, double valorM);
+    double get_M() { return fmasa_real*931.494; }; // La masa esta multiplicada por el factor de cambio kg --> MeV/c² 
     void Print();
 };
 
-void Particula::definir_MZ(double M, double Z)
+void Particula::definir_AZ(double A, double Z, double M)
 {
-    fA = M;
+    fA = A;
     fZ = Z;
-};
+    fmasa_real = M;
+}
 
 void Particula::Print()
 {
@@ -38,20 +40,25 @@ void Particula::Print()
 }
 
 
-// Una colisión esta caracterizada por la energía del atomo incidente y las partículas que la componen
 
+// Una colisión esta caracterizada por la energía del atomo incidente y las partículas que la componen, por lo que es necesario que formen parte de la clase
+//      colisión. Otras que caracterizarían la clase colisión serían: la energía incidente, los ángulos de salida/energías de las partículas resultantes. 
 
 class Colision
 {
-    Particula fp1;
+    Particula fp1; 
     Particula fp2;
     Particula fp3;
     Particula fp4;
     double ftbeam;
 
 public:
-    void energia(double valor);
-    void introducir_particulas(Particula p1, Particula p2, Particula p3, Particula p4);
+    void set_energia(double valor);
+    void set_particulas(Particula p1, Particula p2, Particula p3, Particula p4);
+
+    std::pair<double, double> valores3(Particula p1, Particula p2, Particula p3, Particula p4, double tbeam1, double thetap);
+    std::pair<double, double> valores4(Particula p1, Particula p2, Particula p3, Particula p4, double tbeam1, double thetap);
+
     double get_m1() { return fp1.get_M(); };
     double get_m2() { return fp2.get_M(); };
     double get_m3() { return fp3.get_M(); };
@@ -61,7 +68,7 @@ public:
 };
 
 
-void Colision::introducir_particulas(Particula p1, Particula p2, Particula p3, Particula p4)
+void Colision::set_particulas(Particula p1, Particula p2, Particula p3, Particula p4)
 {
     fp1 = p1;
     fp2 = p2;
@@ -69,7 +76,71 @@ void Colision::introducir_particulas(Particula p1, Particula p2, Particula p3, P
     fp4 = p4;
 };
 
-void Colision::energia(double valor)
+void Colision::set_energia(double valor)
 {
     ftbeam = valor;
+};
+
+
+std::pair<double, double> valores4(Particula par1, Particula par2, Particula par3, Particula par4, double tbeam1, double thetap){
+
+    double m1 {par1.get_M()};
+    double m2 {par2.get_M()};
+    double m3 {par3.get_M()};
+    double m4 {par4.get_M()};
+    double t1 {tbeam1};
+
+    double E1 {m1 + t1};
+    double p1 {pow(pow(E1, 2) - pow(m1, 2), 0.5)};
+    double E2 {m2};
+    double Etot {E1 + m2};
+    double Etotp {pow(pow(Etot, 2) - pow(p1, 2), 0.5)};
+
+    double gamma {Etot / Etotp};
+    double beta {p1 / Etot};
+
+
+    double E3p {(Etotp + (pow(m3, 2) - pow(m4, 2)) / Etotp) / 2};
+    double E4p {(Etotp + (pow(m4, 2) - pow(m3, 2)) / Etotp) / 2};
+
+    double p4p {pow(pow(E4p, 2) - pow(m4, 2), 0.5)};
+
+    double E4 {gamma * (E4p + beta * p4p * TMath::Cos(thetap))};
+    double p4 {pow(-pow(m4, 2) + pow(E4, 2), 0.5)};
+
+    double T4 {E4 - m4};
+    double theta4 {TMath::ACos((gamma / p4) * (p4p * TMath::Cos(thetap) + beta * E4p))};
+    return {T4,theta4};
 }
+
+std::pair<double, double> valores3(Particula par1, Particula par2, Particula par3, Particula par4, double tbeam1, double thetap){
+
+    double m1 {par1.get_M()};
+    double m2 {par2.get_M()};
+    double m3 {par3.get_M()};
+    double m4 {par4.get_M()};
+    double t1 {tbeam1};
+
+    double E1 {m1 + t1};
+    double p1 {pow(pow(E1, 2) - pow(m1, 2), 0.5)};
+    double E2 {m2};
+    double Etot {E1 + m2};
+    double Etotp {pow(pow(Etot, 2) - pow(p1, 2), 0.5)};
+
+    double gamma {Etot / Etotp};
+    double beta {p1 / Etot};
+
+
+    double E3p {(Etotp + (pow(m3, 2) - pow(m4, 2)) / Etotp) / 2};
+    double E4p {(Etotp + (pow(m4, 2) - pow(m3, 2)) / Etotp) / 2};
+
+    double p3p {pow(pow(E3p, 2) - pow(m3, 2), 0.5)};
+
+    double E3 {gamma * (E3p + beta * p3p * TMath::Cos(thetap))};
+    double p3 {pow(-pow(m3, 2) + pow(E3, 2), 0.5)};
+
+    double T3 {E3 - m3};
+    double theta3 {TMath::ACos((gamma / p3) * (p3p * TMath::Cos(thetap) + beta * E3p))};
+    return {T3,theta3};
+}
+
